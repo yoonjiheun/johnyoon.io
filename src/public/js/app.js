42b73a1a35066a9917309364174d0eea0a9cbc9a
John Yoon <John Yoon>
2016-11-17 00:16:30 -0500

    Fixed navbar and changed animations to fade

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,22 @@
-// todo: add hover stuff
+import $ from 'jquery';
+
+$(document).ready(function () {
+	$.fn.extend({
+		tooltip: function () {
+			$(this).mouseover(function (e) {
+				var hoverText = $(this).attr('data-stuff');
+				var x = $('<p>' + hoverText + '</p>')
+				var a = $('<span class="tooltip"></span>');
+
+				a.append(x);
+				$(this).append(a);
+			});
+
+			$(this).mouseout(function (e) {
+				$('body .tooltip').remove();
+			});
+		}
+	});
+
+	$('[data-toggle="tooltip"]').tooltip(); 
+});
